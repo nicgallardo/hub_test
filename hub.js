628791b85c87616217175f78333e512ba6089fb2
Nicholas Gallardo <nic.gallardo@gmail.com>
2017-02-11 03:41:22 -0700

    initial

---
+++ hub.js
@@ -0,0 +1,46 @@
+
+
+var config = {
+  count: -1,
+  iteration: 100,
+  end: 1000
+};
+
+var hub = {
+  writer: (log, string, cb)=>{
+    var fs = require('fs');
+    fs.appendFile(log + '.txt', string, (err) => {
+      if (err) console.log('writing error : \n\t ' + err);
+      cb(null, true);
+    });
+  },
+  kickoff: ()=>{
+
+    if(config.count > config.end -100){
+      console.log('complete hub process');
+      process.exit(1);
+    }else{
+      const execFile = require('child_process').execFile;
+      const child = execFile('node', ['worker.js' , config.count, config.count + config.iteration], (error, stdout, stderr) => {
+        if (error || stderr) {
+          var finalError = error || stderr;
+          hub.writer('error_log', `\n!!!!!!!!!!!!!\n Error occured in count` + config.count + ` :\n \t\t` + finalError +`\n!!!!!!!!!!!!!\n`, (writeErr, writeRes)=>{
+            config.count += 100;
+            hub.kickoff();
+          })
+        }
+
+        if (stdout){
+          hub.writer('write_log', `\n*****\n`+  stdout +`\n*****\n`, (writeErr, writeRes)=>{
+            config.count += 100;
+            hub.kickoff();
+          })
+        };
+
+
+      });
+    }
+  }
+};
+
+hub.kickoff()
---
+++ worker.js
@@ -0,0 +1,17 @@
+var process = require('process');
+var count = process.argv[2],
+    end = process.argv[3];
+// throw 122122
+
+var kickoff = ()=>{
+  count++;
+  // if(count === 50) throw "Some error";
+  if ( count > end){
+    console.log('complete');
+  } else {
+    console.log('ran '+ count);
+    kickoff()
+  };
+};
+
+kickoff();
